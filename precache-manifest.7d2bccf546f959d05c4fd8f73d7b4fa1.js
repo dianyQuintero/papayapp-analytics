4fcbfbc74148eb450926156cb8066a44745112d7
DIANY YISEL QUINTERO PEROZO <dy.quintero@uniandes.edu.co>
2020-04-30 23:59:59 -0500

    Updates

---
+++ precache-manifest.7d2bccf546f959d05c4fd8f73d7b4fa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e08be1df2b4f54ff6a64733f272df452",
+    "url": "/papayapp-analytics/index.html"
+  },
+  {
+    "revision": "047cdbb776ce7c609886",
+    "url": "/papayapp-analytics/static/css/main.67cd550b.chunk.css"
+  },
+  {
+    "revision": "ca4995432aeb3b033a70",
+    "url": "/papayapp-analytics/static/js/2.4f51e8d9.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/papayapp-analytics/static/js/2.4f51e8d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "047cdbb776ce7c609886",
+    "url": "/papayapp-analytics/static/js/main.addc7aeb.chunk.js"
+  },
+  {
+    "revision": "dc68b5e38b816c23a6e6",
+    "url": "/papayapp-analytics/static/js/runtime-main.c2566392.js"
+  }
+]);
